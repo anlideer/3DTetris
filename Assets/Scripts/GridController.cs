63a7849b7fabd7f91e2ff52cf772bce7a3e63d9f
anlideer <anlideer@gmail.com>
2023-04-24 20:51:08 +0300

    Initial commit

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    [Header("Grid panel instance")]
+    public GameObject BottomPanel;
+    public GameObject LeftPanel;
+    public GameObject RightPanel;
+    public GameObject FrontPanel;
+    public GameObject BackPanel;
+
+    [Header("Grid size")]
+    public int GridSizeX;
+    public int GridSizeY;
+    public int GridSizeZ;
+
+
+    void OnDrawGizmos()
+    {
+        // Rescale the grid and panel accordingly
+        if (BottomPanel)
+        {
+            // obj scale and position
+            BottomPanel.transform.localScale = new Vector3((float)GridSizeX / 10, 1, (float)GridSizeZ / 10);
+            BottomPanel.transform.localPosition = new Vector3((float)GridSizeX / 2, 0, (float)GridSizeZ / 2);
+            // material tiliing rescale
+            BottomPanel.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(GridSizeX, GridSizeZ);
+        }
+
+        if (LeftPanel && RightPanel)
+        {
+            // obj scale and position
+            LeftPanel.transform.localScale = new Vector3((float)GridSizeY / 10, 1, (float)GridSizeZ / 10);
+            LeftPanel.transform.localPosition = new Vector3(0, (float)GridSizeY / 2, (float)GridSizeZ / 2);
+            RightPanel.transform.localScale = new Vector3((float)GridSizeY / 10, 1, (float)GridSizeZ / 10);
+            RightPanel.transform.localPosition = new Vector3(GridSizeX, (float)GridSizeY / 2, (float)GridSizeZ / 2);
+            // material tiliing rescale
+            LeftPanel.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(GridSizeY, GridSizeZ);
+        }
+
+        if (FrontPanel && BackPanel)
+        {
+            // obj scale and position
+            FrontPanel.transform.localScale = new Vector3((float)GridSizeY / 10, 1, (float)GridSizeX / 10);
+            FrontPanel.transform.localPosition = new Vector3((float)GridSizeX / 2, (float)GridSizeY / 2, 0);
+            BackPanel.transform.localScale = new Vector3((float)GridSizeY / 10, 1, (float)GridSizeX / 10);
+            BackPanel.transform.localPosition = new Vector3((float)GridSizeX / 2, (float)GridSizeY / 2, GridSizeZ);
+            // material tiliing rescale
+            FrontPanel.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(GridSizeY, GridSizeX);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 3DTetris
+ Basic 3D Tetris, following a course in udemy (my implementation might be different still) https://www.udemy.com/share/104CbC3@8U_lUXth0rxWkUoeuJaFuHB1o6Cf8qz6VyJKoLlVpCfCE5_C3f1Ftqaqlw5djJEBfQ==/
